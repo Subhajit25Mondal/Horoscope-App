56e8c648c556d58514e317bf939b96d738f622c0
Subhajit Mondal <Subhajit25Mondal@users.noreply.github.com>
2018-06-18 12:40:54 +0530

    List Creation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
+import { ResultPage } from '../result/result';
 
 @Component({
   selector: 'page-home',
@@ -9,10 +10,23 @@ import { ContactPage } from '../contact/contact';
 })
 export class HomePage {
 
+  items : any[];
+
 
   constructor(public navCtrl: NavController) {
 
-    
+    this.items=[{ name : 'Aries', date: 'March 21-April 19)'},
+      { name : 'Taurus', date : 'April 20-May 20'},
+      { name : 'Gemini', date : 'May 21-June 20'}, 
+      { name : 'Cancer', date : 'June 21-July 22'},
+      { name : 'Leo', date : 'July 23-August 22'}, 
+      { name : 'Virgo', date : 'August 23-September 22'},      
+      { name : 'Libra', date : 'September 23-October 22'},     
+      { name : 'Scorpio', date : 'October 23-November 21'},     
+      { name : 'Sagittarius', date : 'November 22-December 21'},     
+      { name : 'Capricorn', date : 'December 22-January 19'},
+      { name : 'Aquarius', date : 'January 20-February 18'},  
+      { name : 'Pisces', date : 'February 19 to March 20'}];
 }
 
 openAbout() {
@@ -24,6 +38,10 @@ openAbout() {
 openContact() {
 
     this.navCtrl.push(ContactPage);
+}
+
+openResults(name :String) {
+  this.navCtrl.push(ResultPage,name);
 }
 
 }
