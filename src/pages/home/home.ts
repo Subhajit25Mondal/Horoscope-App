084c6f2f8ec6a4dfa5d00b7de4615f895f0bb0a8
Subhajit Mondal <Subhajit25Mondal@users.noreply.github.com>
2018-06-18 13:41:35 +0530

    FrontPage UI Improvements

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,18 +15,18 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
-    this.items=[{ name : 'Aries', date: 'March 21-April 19', img : 'Aries.png'},
-      { name : 'Taurus', date : 'April 20-May 20', img : 'Taurus.png'},
-      { name : 'Gemini', date : 'May 21-June 20', img : 'Gemini.png'}, 
-      { name : 'Cancer', date : 'June 21-July 22', img : 'Cancer.png'},
-      { name : 'Leo', date : 'July 23-August 22', img : 'Leo.png'}, 
-      { name : 'Virgo', date : 'August 23-September 22', img : 'Virgo.png'},      
-      { name : 'Libra', date : 'September 23-October 22', img : 'Libra.png'},     
-      { name : 'Scorpio', date : 'October 23-November 21', img : 'Scorpio.png'},     
-      { name : 'Sagittarius', date : 'November 22-December 21', img : 'Sagittarius.png'},     
-      { name : 'Capricorn', date : 'December 22-January 19', img : 'Capricorn.png'},
-      { name : 'Aquarius', date : 'January 20-February 18', img : 'Aquarius.png'},  
-      { name : 'Pisces', date : 'February 19 to March 20', img : 'Pisces.png'}];
+    this.items=[{ name : 'Aries', date: 'Mar 21 - Apr 19', img : 'Aries.png'},
+      { name : 'Taurus', date : 'Apr 20 - May 20', img : 'Taurus.png'},
+      { name : 'Gemini', date : 'May 21 - June 20', img : 'Gemini.png'}, 
+      { name : 'Cancer', date : 'June 21 - July 22', img : 'Cancer.png'},
+      { name : 'Leo', date : 'July 23 - Aug 22', img : 'Leo.png'}, 
+      { name : 'Virgo', date : 'Aug 23 - Sept 22', img : 'Virgo.png'},      
+      { name : 'Libra', date : 'Sept 23 - Oct 22', img : 'Libra.png'},     
+      { name : 'Scorpio', date : 'Oct 23 - Nov 21', img : 'Scorpio.png'},     
+      { name : 'Sagittarius', date : 'Nov 22 - Dec 21', img : 'Sagittarius.png'},     
+      { name : 'Capricorn', date : 'Dec 22 - Jan 19', img : 'Capricorn.png'},
+      { name : 'Aquarius', date : 'Jan 20 - Feb 18', img : 'Aquarius.png'},  
+      { name : 'Pisces', date : 'Feb 19 - Mar 20', img : 'Pisces.png'}];
 }
 
 openAbout() {
