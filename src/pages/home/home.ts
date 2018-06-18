d6dd5641b2be03218550557fb887b161da1da4e5
Subhajit Mondal <Subhajit25Mondal@users.noreply.github.com>
2018-06-18 13:24:09 +0530

    HomePage UI Improvements

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -21,10 +21,10 @@ export class HomePage {
       { name : 'Cancer', date : 'June 21-July 22', img : 'Cancer.png'},
       { name : 'Leo', date : 'July 23-August 22', img : 'Leo.png'}, 
       { name : 'Virgo', date : 'August 23-September 22', img : 'Virgo.png'},      
-      { name : 'Libra', date : 'September 23-October 22', img : 'Libre.png'},     
+      { name : 'Libra', date : 'September 23-October 22', img : 'Libra.png'},     
       { name : 'Scorpio', date : 'October 23-November 21', img : 'Scorpio.png'},     
       { name : 'Sagittarius', date : 'November 22-December 21', img : 'Sagittarius.png'},     
-      { name : 'Capricorn', date : 'December 22-January 19', img : 'caprocorn.png'},
+      { name : 'Capricorn', date : 'December 22-January 19', img : 'Capricorn.png'},
       { name : 'Aquarius', date : 'January 20-February 18', img : 'Aquarius.png'},  
       { name : 'Pisces', date : 'February 19 to March 20', img : 'Pisces.png'}];
 }
